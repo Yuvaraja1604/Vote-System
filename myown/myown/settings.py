0c9e9111963127de621652b69c0b44a77ec79edb
Yuvaraja1604 <95240086+Yuvaraja1604@users.noreply.github.com>
2024-04-06 19:44:12 +0530

    settings.py

--- myown/myown/settings.py
+++ myown/myown/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm_+b+$0ctnw2x+i%t%+bt+v##52hx9ccjl%c42q7+c#8id2+0k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
